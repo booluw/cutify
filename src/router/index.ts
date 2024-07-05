3538c30c5708751de694d4fa26a5b1df90ebbaab
= <=>
2024-07-05 11:34:02 +0100

    feat: cmpleted error404 page

--- src/router/index.ts
+++ src/router/index.ts
@@ -45,13 +45,10 @@ const router = createRouter({
       ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/:pathMatch(.*)*',
+      name: 'Error404Page',
+      component: () => import('@/views/404.vue')
+    },
   ]
 })
 
---
+++ src/views/404.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import AppButton from '@/components/AppButton.vue'
+</script>
+<template>
+  <section class="h-screen flex flex-col gap-[10px] items-center justify-center">
+    <h1 class="text-[32px] fonr-bold">This page is not found.</h1>
+    <router-link to="/">
+      <AppButton type="solid" size="large">Go Back Home</AppButton>
+    </router-link>
+  </section>
+</template>
